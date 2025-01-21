b421141d0c1fd4fe94f9a5509dcdc12d468b24fc
Drew Allred <drew.e.allred@gmail.com>
2025-01-21 09:23:36 -0500

    Added dynamic age for modal

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 
-// JS
+
+// DYNAMIC AGE FOR MODAL
+
+const birthday = '2005-07-09'
+const ageReference = document.getElementsByClassName('age')[0]
+
+function setAge() {
+
+    let today = new Date()
+    let dob = new Date( birthday )
+
+    let age = today.getFullYear() - dob.getFullYear()
+    let m = today.getMonth() - dob.getMonth()
+    let d = today.getDate() - dob.getDate()
+
+    if ( m < 0 || ( m == 0 && d <= 0 ) ) {
+        age--
+    }
+
+    ageReference.innerHTML = age
+
+}
+
+setAge()
+
+
+// JAVASCRIPT
+
