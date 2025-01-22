eef427986b8cdc1dffac3b03815fb81c09bb8b8d
Drew Allred <drew.e.allred@gmail.com>
2025-01-22 11:39:44 -0500

    Background shapes, scroll pausing, various fixes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-// DYNAMIC AGE FOR MODAL
+/* DYNAMIC AGE */
 
 const birthday = '2005-07-09'
 const ageReference = document.getElementsByClassName('age')[0]
@@ -25,7 +25,7 @@ function setAge() {
 setAge()
 
 
-// EMAIL FUNCTIONALITY FOR MODAL
+/* EMAIL */
 
 function contact(event) {
 
@@ -61,17 +61,20 @@ function contact(event) {
 }
 
 
-// SHOW FUNCTIONALITY FOR MODAL
+/* MODAL */
 
 let modalOpen = false
 
 function toggleModal() {
 
     if (!modalOpen) {
+        for (let i = 0; i < shapes.length; i++) { shapes[i].style.translate = '0 0' }
         document.getElementsByTagName('body')[0].classList += ' modal--open'
         document.getElementsByClassName('modal')[0].style.zIndex = 3
+        disableScroll()
     } else {
         document.getElementsByTagName('body')[0].classList.remove('modal--open')
+        enableScroll()
         setTimeout(
             () => {
                 document.getElementsByClassName('modal')[0].style.zIndex = -1
@@ -87,7 +90,7 @@ function toggleModal() {
 
 
 
-// DARK MODE
+/* DARK MODE */
 
 let darkToggle = false
 
@@ -100,5 +103,110 @@ function darkMode() {
     } else {
         document.body.classList.remove('dark-mode')
     }
+
+}
+
+darkMode()
+
+/* RANDOMIZE SHAPE ROTATIONS */
+
+let shapes = document.getElementsByClassName('shape')
+let shapeSpeedRange = [4, 25]
+let randDirection = true
+
+function range(start, end) {
+    var ans = [];
+    for (let i = start; i <= end; i++) {
+        ans.push(i);
+    }
+    return ans;
+}
+
+shapeSpeedRange = range(shapeSpeedRange[0], shapeSpeedRange[1])
+
+function randShapeRot() {
+
+    for (let i = 0; i < shapes.length; i++) {
+
+        let dur = shapeSpeedRange[Math.floor(Math.random() * shapeSpeedRange.length)]
+        shapes[i].style.animationDuration = dur + 's'
+
+        if (randDirection && Math.floor(Math.random() * 2) == 0) {
+            shapes[i].style.animationDirection = 'reverse'
+        }
+    }
+}
+
+randShapeRot()
+
+
+/* MOUSE INTERACT */
+
+let scaleFactor = 30
+let shapeLocs = [
+    [10, 40],
+    [25, 4],
+    [-30, 35],
+    [-45, 0],
+    [-20, 15],
+    [50, 1],
+    [0, 0],
+    [40, 20]
+]
+
+function moveShapes(event) {
+
+    if (!modalOpen) {
+        const x = event.clientX / scaleFactor
+        const y = event.clientY / scaleFactor
+
+        for (let i = 0; i < shapes.length; i++) {
+
+            let chance = i % 4
+
+            if (chance == 0) {
+                shapes[i].style.translate = `${x}px ${y}px`
+            } else if (chance == 1) {
+                shapes[i].style.translate = `-${x}px ${y}px`
+            } else if (chance == 2) {
+                shapes[i].style.translate = `${x}px -${y}px`
+            } else {
+                shapes[i].style.translate = `-${x}px -${y}px`
+            }
+
+        }
+    } else if (true) {
+
+        var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+        if (window.innerHeight / 2 > scrollTop) {
+            for (let i = 0; i < shapes.length; i++) {
+                let top = shapeLocs[i][1] + scrollTop
+                shapes[i].style.translate = shapeLocs[i][0] + 'px ' + top + 'px'
+            }
+        } else {
+            for (let i = 0; i < shapes.length; i++) {
+                shapes[i].style.translate = '0 -90000vh'
+            }
+        }
+    }
+
+}
+
+
+/* SCROLL */
+
+function disableScroll() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+    window.onscroll = function () {
+        window.scrollTo({ top: scrollTop, behavior: 'instant' })
+    }
+
+}
+
+function enableScroll() {
+
+    window.onscroll = function () { }
 
 }
